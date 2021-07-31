28746912770883509c0ecfd4ecae8f4845cf8713
Mary <mariyapiligrim3@gmail.com>
2021-07-31 16:55:39 +0300

    Fix some tasks

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-
 class Worker {
   constructor(name, lastName, days, salary, premium) {
+    if (isNaN(days) || typeof days !== "number") {
+      throw new TypeError("Days must be number");
+    }
 
-    if(salary < 0) {
-      throw new TypeError ('Salary must be positive');
+    if (isNaN(salary)|| typeof salary !== "number") {
+      throw new TypeError("Salary must be number");
     }
-    if(days < 0) {
-      throw new TypeError ('Days must be positive');
+    if (salary < 0) {
+      throw new TypeError("Salary must be positive");
     }
-    if(premium === true) {
-      salary = salary + (salary * 0.25);
+    if (days < 0) {
+      throw new TypeError("Days must be positive");
+    }
+    if (premium === true) {
+      salary += salary * 0.25;
     }
 
     this.name = name;
@@ -27,5 +32,5 @@ class Worker {
   }
 }
 
-const worker1 = new Worker('Test', 'Testovich', 30, 300, true);
-const worker2 = new Worker('Text', 'Textovich', 15, 150, false);
+const worker1 = new Worker("Test", "Testovich", 30, 300, true);
+const worker2 = new Worker("Text", "Textovich", 15, 150, false);
