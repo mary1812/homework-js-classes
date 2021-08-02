0f50eee521c9428d5a5e33f6e28f8543d5f6fd1c
Mary1812 <mariyapiligrim3@gmail.com>
2021-08-02 14:42:08 +0300

    Add PREMIUM_COEFFICIENT

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
+const PREMIUM_COEFFICIENT = 1.25;
+
 class Worker {
   constructor(name, lastName, days, salary, premium) {
     if (isNaN(days) || typeof days !== "number") {
       throw new TypeError("Days must be number");
     }
-
-    if (isNaN(salary)|| typeof salary !== "number") {
+    if (isNaN(salary) || typeof salary !== "number") {
       throw new TypeError("Salary must be number");
     }
     if (salary < 0) {
       throw new TypeError("Salary must be positive");
-    }
-    if (days < 0) {
-      throw new TypeError("Days must be positive");
-    }
-    if (premium === true) {
-      salary += salary * 0.25;
     }
 
     this.name = name;
@@ -28,9 +23,12 @@ class Worker {
     return `${this.name} ${this.lastName}`;
   }
   getSalary() {
+    if (this.premium) {
+      return this.days * this.salary * PREMIUM_COEFFICIENT;
+    }
     return this.days * this.salary;
   }
 }
 
-const worker1 = new Worker("Test", "Testovich", 30, 300, true);
-const worker2 = new Worker("Text", "Textovich", 15, 150, false);
+const worker1 = new Worker("Test", "Testovich", 30, 100, true);
+const worker2 = new Worker("Text", "Textovich", 15, 50, false);
